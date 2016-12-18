7fa98085750b15ab28ee0fa5700b7b6dc4b7d4c2
alexanderwjrussell <awjrussell@live.co.uk>
2016-12-18 18:58:26 +0000

    Console Project 1) Created FizzBuzz console project to output 0 - 99 in FizzBuzz

---
+++ FizzBuzzConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FizzBuzz_CS;
+
+namespace FizzBuzzConsole
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Console.WriteLine(FizzBuzz.Get(i));
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
